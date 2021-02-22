ea63c44c1fb7c2c60f1a5ac5e56f492691ce841d
Kai-Wei Lin <200436558@student.georgianc.on.ca>
2021-02-23 03:36:28 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Penetration_Testing_Hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
