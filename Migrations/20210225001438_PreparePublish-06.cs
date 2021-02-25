ae4fc16fb363ca633b45556513ed583de5735fcb
Kai-Wei Lin <200436558@student.georgianc.on.ca>
2021-02-25 10:46:41 +0800

    First Public

---
+++ Migrations/20210225001438_PreparePublish-06.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Penetration_Testing_Hub.Migrations
+{
+    public partial class PreparePublish06 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
